36b1342c24017f6f48e6ec2629bd86d17677accb
adnan <adnan.ozturk@ogr.sakarya.edu.tr>
2025-11-28 10:44:22 +0300

    Page moved to LoginForm component

--- src/component/LoginForm.jsx
+++ src/component/LoginForm.jsx
@@ -1,11 +1,15 @@
 import Form from "./Form.jsx";
-import {Button, Stack, TextField, Typography} from "@mui/material";
+import {Button, Paper, Stack, TextField, Typography} from "@mui/material";
 import {useForm} from "react-hook-form";
+import {useNavigate} from "react-router";
 
 function LoginForm() {
 
+    const navigate = useNavigate();
+
     const handleClickButton = async (data) => {
-        console.log(data)
+        console.log(data);
+        navigate("/game");
     }
 
     const {
@@ -14,23 +18,36 @@ function LoginForm() {
         formState: {errors}
     } = useForm({defaultValues: {username: ""}})
 
+
+    // Burada hata mesajlarını helperText içerisine değil de, paper içerisine alert olarak ekleyebiliriz :)
     return (
-        <Form onSubmit={handleSubmit(handleClickButton)}>
-            <Stack direction="row" spacing={2} justifyContent='space-between' alignItems="center" sx={{m: 7}}>
-                <Typography>İsminizi Giriniz</Typography>
-                <TextField
-                    {...register("username",{
-                        required:"Kullanıcı adi girmeniz zorunludur!",
-                        minLength:3,
-                        maxLength:25
-                    })}
-                    label="username"
-                    error={!!errors.username}
-                    helperText={errors.username?.message}
-                    variant="outlined"/>
-            </Stack>
-            <Button variant="contained" fullWidth size="large" color="primary" type="submit">Başla</Button>
-        </Form>
+        <Paper sx={{padding: 2, backgroundColor: 'white'}}>
+            <Typography variant="h4" align="center" sx={{fontWeight: "bold", mb: 3, mt: 3}}>Milyoner'e Hoşgeldiniz
+                :)</Typography>
+            <Form onSubmit={handleSubmit(handleClickButton)}>
+                <Stack direction="row" spacing={2} justifyContent='space-between' alignItems="center" sx={{m: 7}}>
+                    <Typography>İsminizi Giriniz</Typography>
+                    <TextField
+                        {...register("username", {
+                            required: "Kullanıcı adi girmeniz zorunludur!",
+                            minLength: {
+                                value: 3,
+                                message: "İsminizin uzunluğu en az 3 birim olmalıdır !"
+                            },
+                            maxLength: {
+                                value: 25,
+                                message: "İsminizin uzunluğu en fazla 25 birim olabilir !"
+                            }
+                        })}
+                        label="username"
+                        error={!!errors.username}
+                        helperText={errors.username?.message}
+                        variant="outlined"/>
+                </Stack>
+                <Button variant="contained" fullWidth size="large" color="primary" type="submit">Başla</Button>
+            </Form>
+        </Paper>
+
     );
 }
 
--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,15 +1,10 @@
-import {Container, Paper, Typography} from "@mui/material";
+import {Container} from "@mui/material";
 import LoginForm from "../component/LoginForm.jsx";
 
 function LoginPage() {
     return (
         <Container maxWidth={"md"}>
-            <Paper sx={{padding: 2, backgroundColor: 'white'}}>
-                <Typography variant="h4" align="center" sx={{fontWeight: "bold", mb: 3, mt: 3}}>Milyoner'e Hoşgeldiniz
-                    :)</Typography>
-                <LoginForm/>
-
-            </Paper>
+            <LoginForm/>
         </Container>
     )
 }
