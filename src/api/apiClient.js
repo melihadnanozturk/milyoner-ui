687f80156561f57cfaca8298bb23ea99132f3382
M.Adnan <adnanmelihozturk@gmail.com>
2025-12-16 12:05:06 +0300

    isAuthExcluded method created

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -9,14 +9,29 @@ export const setStore = (storeInstance) => {
     injectedStore = storeInstance;
 }
 
+const isAuthExcluded = (config) => {
+    // Per-request override: axios.get(url, { skipAuth: true })
+    console.log("isAuthExcluded", config);
+
+    if (config?.skipAuth === true) return true;
+}
+
 axios.interceptors.request.use(
     (config) => {
+        if (isAuthExcluded(config)) {
+            console.log("AUTH_SKIPPED");
+            if (config?.headers?.Authorization) {
+                delete config.headers.Authorization;
+            }
+            return config;
+        }
+
+
         const store = injectedStore.getState();
         // TODO: token'ı hangi slice'ta tutuyorsan burayı değiştir
         // örn: const token = state.auth?.token;
         const token = store.game?.token;
 
-        console.log("REDUX_TOKEN", token);
 
         if (token) {
             config.headers = config.headers ?? {};
@@ -31,7 +46,7 @@ axios.interceptors.request.use(
 
 const methods = {
     get: (url) => axios.get(url).then((response) => response.data),
-    post: (url, body) => axios.post(url, body).then((response) => response.data),
+    post: (url, body, config) => axios.post(url, body, config).then((response) => response.data),
     put: (url, body) => axios.put(url, body).then((response) => response.data),
     delete: (url) => axios.delete(url).then((response) => response.data),
 }
@@ -45,7 +60,7 @@ const gameplay = {
 }
 
 const panelAuth = {
-    login: (username, password) => methods.post("panel/auth/login", username, password)
+    login: (body) => methods.post("panel/auth/login", body, {skipAuth : true})
 }
 
 const panelOperation = {
--- src/component/AdminLoginForm.jsx
+++ src/component/AdminLoginForm.jsx
@@ -1,22 +1,20 @@
 import Form from "./Form.jsx";
 import {Button, Paper, Stack, TextField, Typography} from "@mui/material";
 import {useForm} from "react-hook-form";
-import {useNavigate} from "react-router";
 import {useDispatch} from "react-redux";
-import {fetchStartGame} from "../page/slices/GameSlice.js";
+import {fetchAdminLogin} from "../page/panel/slice/AdminAuthSlice.js";
 
-function LoginForm() {
+function AdminLoginForm() {
     const dispatch = useDispatch();
-    const navigate = useNavigate();
 
     const handleClickButton = async (data) => {
         const body = {
-            username: data.username
+            username: data.username,
+            password: data.password,
         }
         try {
             //burada hata olabilir
-            await dispatch(fetchStartGame(body)).unwrap();
-            navigate("/game");
+            await dispatch(fetchAdminLogin(body)).unwrap();
         } catch (error) {
             alert("INTERNAL_SERVER_ERROR :: " + error.message)
         }
@@ -32,11 +30,11 @@ function LoginForm() {
     // Burada hata mesajlarını helperText içerisine değil de, paper içerisine alert olarak ekleyebiliriz :)
     return (
         <Paper sx={{padding: 2, backgroundColor: 'white'}}>
-            <Typography variant="h4" align="center" sx={{fontWeight: "bold", mb: 3, mt: 3}}>Milyoner'e Hoşgeldiniz
-                :)</Typography>
+            <Typography variant="h4" align="center" sx={{fontWeight: "bold", mb: 3, mt: 3}}>Maoco Milyoner Admin
+                Panel </Typography>
             <Form onSubmit={handleSubmit(handleClickButton)}>
                 <Stack direction="row" spacing={2} justifyContent='space-between' alignItems="center" sx={{m: 7}}>
-                    <Typography>İsminizi Giriniz</Typography>
+                    <Typography>İsmi</Typography>
                     <TextField
                         {...register("username", {
                             required: "Kullanıcı adi girmeniz zorunludur!",
@@ -53,12 +51,27 @@ function LoginForm() {
                         error={!!errors.username}
                         helperText={errors.username?.message}
                         variant="outlined"/>
+
                 </Stack>
-                <Button variant="contained" fullWidth size="large" color="primary" type="submit">Başla</Button>
+
+                {/*todo: burada passwordun gözükmemesi gerekir*/}
+                <Stack direction="row" spacing={2} justifyContent='space-between' alignItems="center" sx={{m: 7}}>
+                    <Typography>Şifre</Typography>
+                    <TextField
+                        {...register("password", {
+                            required: "Şİfre girmeniz zorunludur!",
+                            //todo: password için must have ibareleri gereklidir
+                        })}
+                        label="password"
+                        error={!!errors.password}
+                        helperText={errors.password?.message}
+                        variant="outlined"/>
+                </Stack>
+                <Button variant="contained" fullWidth size="large" color="primary" type="submit">Giriş Yap</Button>
             </Form>
         </Paper>
 
     );
 }
 
-export default LoginForm;
+export default AdminLoginForm;
